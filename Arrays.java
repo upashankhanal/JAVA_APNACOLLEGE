4c8151bd9519ac68fe8f8a5ab84632b4d23bbb68
upashana.learning <khanalupashana2060@gmail.com>
2025-03-26 21:33:57 -0400

    Making an array taking data from users

---
+++ Arrays.java
@@ -0,0 +1,21 @@
+// taking input from a user and placing it in an array
+
+import java.util.*;
+public class Arrays {
+
+    public static void main(String args[]){
+     int numb[]= new int[60];
+      Scanner sc= new Scanner(System.in);
+      numb[0]= sc. nextInt();
+      numb[1]= sc.nextInt();
+      numb[2]=sc.nextInt();
+      System.out.print("the first numb is :" + numb[0]);
+      System.out.println("the second numb is :" + numb[1]);
+      System.out.println("the third numb is :" + numb[2]);
+
+
+
+
+
+    }
+}
