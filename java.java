ccbf91e36f67ab77fb6e45ad0e2e7d64873e1ece
upashana.learning <khanalupashana2060@gmail.com>
2025-03-26 21:15:03 -0400

    Making an array

---
+++ java.java
@@ -0,0 +1,10 @@
+public class java{
+
+
+    public static void main(String args[]){
+        int numb[]= {1,2,3,4,5,6};
+        for(int i=0; i<numb.length; i++){
+            System.out.print(numb[i]);
+        }
+    }
+}
